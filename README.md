2f1f9417fc0c66ee3058de8cb6e2a138714ea112
Dot32 <61964090+Dot32Dev@users.noreply.github.com>
2026-03-20 01:10:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Terrain
+## Build from fresh Git clone instructions
+To build, make a folder in the project directory named third_party, inside 
+create a folder named GLFW, download GLFW binaries and copy your version of 
+visual studio's folder's contents into your GLFW folder. Add its include 
+directory to your project. Similarly with GLAD, generate a GLAD for 3.3 core 
+profile, and copy its files into a GLAD subfolder of the third_party folder.
+Compile in Visual Studio (2022)
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+const int WIN_W = 800;
+const int WIN_H = 600;
+
+void resize(GLFWwindow* window, int width, int height);
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WIN_W, WIN_H, "Terrain", NULL, NULL);
+	if (window == NULL) return -1;
+	
+	glfwMakeContextCurrent(window);
+	
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -2;
+
+	resize(window, WIN_W, WIN_H);
+	glfwSetFramebufferSizeCallback(window, resize);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void resize(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
