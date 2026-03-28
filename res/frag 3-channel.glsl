b251609624b5aa6b0783a1e757ec836bc9eee630
Dot32 <61964090+Dot32Dev@users.noreply.github.com>
2026-03-28 13:39:54 +0800

    Make texture tile, far nicer to look at

--- res/frag 3-channel.glsl
+++ res/frag 3-channel.glsl
@@ -6,7 +6,7 @@ in vec2 uv;
 uniform sampler2D texture_in;
 
 void main() {
-	vec3 val = texture(texture_in, uv).rgb;
+	vec3 val = texture(texture_in, uv * 32.0).rgb;
 	// frag_colour = vec4(val, 1.0);
 	frag_colour = vec4(val, 1.0);
 }
